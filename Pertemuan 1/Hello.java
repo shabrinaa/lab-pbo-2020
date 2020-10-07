55d52491a2cf1a03623d76170ea04e859853446a
unknown <Shabrina Miftahul Aida>
2020-10-07 20:59:20 +0700

    Membuat Kode Pertemuan 1

---
+++ Pertemuan 1/Hello.java
@@ -0,0 +1,13 @@
+class Hello {
+    public static void main(String[]args){
+        System.out.println("Hello");
+    }
+}
+
+/* java adalah jenis pemrograman yang berbasis objek, oleh karena itu setiap programnya harus diawali dengan 
+class agar dapat dijadikan objek. cara mendeklarasikan class yaitu" class NamaProgram{ .
+selanjutnya fungsi main() harus ditulis agar program dapat dijalankan.fungsi main()
+tidak dapat dikenali tanpa parameter string[] args. string[] args berfungsi untuk menerima argumen yang diberi
+user kepada program. argumen yang diberikan disini adalah Hello. System.out.println berfungsi untuk
+menampilkan argumen atau teks pada layar monitor. 
+*/
